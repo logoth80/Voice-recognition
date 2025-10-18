855296d0479c2dc10b4280d647800b00ac850a68
logoth80 <nowakowskilogoth@gmail.com>
2025-10-18 13:43:53 +0200

    v000002

--- voice.py
+++ voice.py
@@ -1,10 +1,11 @@
 import speech_recognition
-import pyttsx3
-from pynput.keyboard import Controller
+#import pyttsx3
+import pyautogui
+from pynput.keyboard import Controller, Key
 import time
 
 recognizer = speech_recognition.Recognizer()
-engine = pyttsx3.init()
+# engine = pyttsx3.init()
 keyboard = Controller()
 
 known_words_nomod = {
@@ -20,27 +21,39 @@ known_words_nomod = {
 }
 known_words_control={
     "check": "d"
-
 }
 
-def press_keyboard(button, modifier=None, duration=0.1):
-        
+def press_keyboard(button, duration=0.1):        
     keyboard.press(button)
+    print(f"pressing {button}, for {duration} seconds") 
     time.sleep(duration)
     keyboard.release(button)
 
-    print(f"pressing {button}, for {duration} seconds")
+def press_mouse(b="left"):
+    pyautogui.click(button=b)
 
-def press_combo(modifier, button, duration=0.1):
+def vc_control():
+    print("ctrl")
+    if keyboard.ctrl_pressed:
+        print("released ctrl")
+        keyboard.release(Key.ctrl)
+    else:
+        print("pressed ctrl")
+        keyboard.press(Key.ctrl)
 
-    print(f"pressing {modifier} + {button}")
+def vc_shift():
+    print("shift")
+    if keyboard.shift_pressed:
+        print("released shift")
+        keyboard.release(Key.shift)
+    else:
+        print("pressed shift")
+        keyboard.press(Key.shift)
 
 while True:
     try:
         with speech_recognition.Microphone(device_index=2) as mic:
             print("listening...")
-            #recognizer.adjust_for_ambient_noise(mic,duration=0.2)
-            #audio=recognizer.listen(mic)
             audio=recognizer.listen(mic, phrase_time_limit=2)
             text=recognizer.recognize_google(audio)
             print(text)
@@ -54,18 +67,26 @@ while True:
             if last_char.isdigit():
                 if last_char=="1":
                     press="q"
+                    press_keyboard(press)
                 elif last_char=="2":
                     press="w"
+                    press_keyboard(press)
                 elif last_char=="3":
                     press="e"
+                    press_keyboard(press)
                 elif last_char=="4":
                     press="r"
+                    press_keyboard(press)
                 elif last_char=="5":
                     press="t"
+                    press_keyboard(press)
+                elif last_char=="6":
+                    press_mouse("middle")
+                elif last_char=="7":
+                    press_mouse("right")
                 else:
                     press=last_char
-
-                press_keyboard(press)
+                    press_keyboard(press)
 
             # 2) Otherwise, check the last word
             else:
@@ -74,6 +95,11 @@ while True:
                     key = known_words_nomod[last_word]
                     press_keyboard(key)
                     print(f"Pressed key: {key}")
+                elif last_word=="control":
+                    vc_control()
+                elif last_word=="enchanting":
+                    vc_shift()
+                    
                     
     except speech_recognition.UnknownValueError:
         recognizer=speech_recognition.Recognizer()
